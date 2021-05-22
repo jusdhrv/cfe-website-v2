1d243f534b4ea045d65047f0129b9fd2b3057920
AndroDev21 <20d.dhruv@gmail.com>
2021-05-22 13:38:28 +0530

    basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { Error404Component } from './error404/error404.component';
 const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: '**', component: Error404Component}
 ];
 
